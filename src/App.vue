81ab5775c4a0c23953bac08bf35bee507d535f81
Myron Apostolakis <myron.apostolakis@sunlight.io>
2024-06-29 23:31:03 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
