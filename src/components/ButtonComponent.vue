a199aae2816d3c7145ac8dc66fa87f9e7b84f0df
Myron Apostolakis <myron.apostolakis@sunlight.io>
2024-07-06 19:39:23 +0300

    removed defineProps macro

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -18,7 +18,6 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue'
 import { EVENTS, VARIANTS, SIZES, ICON_PLACEMENT } from './constants'
 import { type ButtonProps } from './types'
 
